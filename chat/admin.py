b4aece2c4532749f56da1a43957fc4c245cfcc7e
faith-ware <faithade69@gmail.com>
2021-07-31 01:32:14 +0100

    Added connected_channel model and online model to admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Group,Chat, Member
+from .models import Group,Chat, Member, Connected_channel, Online
 # Register your models here.
 
 admin.site.register(Group)
 admin.site.register(Chat)
 admin.site.register(Member)
+admin.site.register(Connected_channel)
+admin.site.register(Online)
 
