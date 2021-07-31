7fbe202e3c7f4960fee204c212253ae73abcedec
faith-ware <faithade69@gmail.com>
2021-07-31 01:21:52 +0100

    Added delete user channel path

--- chat/urls.py
+++ chat/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("chat/<str:room_name>/", views.room, name="room"),
     path("generate/", views.generate_group_name, name="generate"),
     path("userlogin/<str:room_name>/", views.user_auth, name="user_auth"),
+    path("delete_channel/<str:room_name>/", views.delete_user_channel, name="delete_user_channel")
 ]
