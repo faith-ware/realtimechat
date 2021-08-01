9b3cb53961757f52e207e9ef29491c98f11420c5
faith-ware <faithade69@gmail.com>
2021-08-02 00:28:54 +0100

    Made some changes for deployment

--- realtimechat/settings.py
+++ realtimechat/settings.py
@@ -16,8 +16,6 @@ import django_heroku
 import dj_database_url
 from decouple import config
 
-from dotenv import load_dotenv
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
