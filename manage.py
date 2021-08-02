b4bc9812eeae88ed9a86af8dc30fd4b46017c051
faith-ware <faithade69@gmail.com>
2021-08-02 09:34:10 +0100

    Made some changes

--- manage.py
+++ manage.py
@@ -20,4 +20,6 @@ def main():
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtimechat.settings')
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
     main()
