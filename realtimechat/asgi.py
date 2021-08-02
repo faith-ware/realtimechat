ce0aae83e452bb1da32c395fd56a8f49f370f1de
faith-ware <faithade69@gmail.com>
2021-08-02 10:00:19 +0100

    Made some changes

--- realtimechat/asgi.py
+++ realtimechat/asgi.py
@@ -16,6 +16,7 @@ import chat.routing
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtimechat.settings')
+django.setup()
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
