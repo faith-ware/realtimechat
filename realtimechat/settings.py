045a33e45cbcbdc4185b87bb68b492d7827ed257
faith-ware <faithade69@gmail.com>
2021-08-02 13:59:11 +0100

    Hid secret key

--- realtimechat/settings.py
+++ realtimechat/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-izeyez61rnsl0z$2_oy5_&b6@4+77fgmbtf9n(6k!!5tmnj5^n'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
